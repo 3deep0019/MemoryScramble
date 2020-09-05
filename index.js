22cceeb458da9ee398610012a275c469c7b98e3e
3deep0019 <trideep@LAPTOP-U7JART73.localdomain>
2020-09-06 03:12:48 +0530

    modification

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const $playButton = document.getElementById('play');
+const $step1Skip = document.getElementById('step1button')
+const $step2Next = document.getElementById('step2')
+const $step1Gone = document.getElementById('step1')
 
 $playButton.onclick = () => {
     location.href = "game.html";
+}
+
+$step1Skip.onclick = () => {
+    $step1Gone.style.display = "none"
+    $step2Next.style.display = "flex"
 }
